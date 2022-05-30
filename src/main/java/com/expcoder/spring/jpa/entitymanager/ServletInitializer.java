5cf486541a16d25c9613c517142b72a9253c8b65
Kabirul Islam Mondal <kabirul05@gmail.com>
2022-05-30 12:01:07 +0530

    Add files via upload

---
+++ src/main/java/com/expcoder/spring/jpa/entitymanager/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.expcoder.spring.jpa.entitymanager;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringJpaEntitymanagerApplication.class);
+	}
+
+}
---
+++ src/test/java/com/expcoder/spring/jpa/entitymanager/SpringJpaEntitymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expcoder.spring.jpa.entitymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaEntitymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
